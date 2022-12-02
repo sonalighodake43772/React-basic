465bfe9ddf7207552a281dc5e13ca0106e47d094
sonalighodake43772 <sonalighodake867>
2022-12-02 13:01:19 +0530

    add cmponent date and details

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,21 +1,18 @@
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
-function ExpenseItem(props)
-{
 
-    
-    return (
-        <div className='expense-item'>
-        <div>{props.data.date.toISOString()}</div>
-        <div className='expense-item__description'>
-            <h2>{props.data.title}</h2>
-        </div>
-        <div className='expense-item__description'>
-            <h2>{props.data.LocationOfExpenditure}</h2>
-        </div>
-        <div className='expense-item__price'>{props.data.amount}</div>
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      {/* <ExpenseDetails title={props.title} />
+      <ExpenseDetails LocationOfExpenditure={props.LocationOfExpenditure} /> */}
+      <ExpenseDetails amount={props.amount} />
+      
 
-       
-        </div>
-    );
+      
+    </div>
+  );
 }
 export default ExpenseItem;
