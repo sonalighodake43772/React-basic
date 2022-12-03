3da01db6a7ea5ab54237a60be4489a33bd8c6bc4
sonalighodake43772 <sonalighodake867>
2022-12-03 12:44:50 +0530

    expensedetails js

---
+++ src/component/Expenses/ExpenseDetails.js
@@ -0,0 +1,18 @@
+import './ExpenseItem.css';
+const ExpenseDetails=(props)=>
+{
+    return(
+        
+             <div className='expense-item'>
+                {/* <div className="expense-item__description">
+            <h2>{props.title}</h2>
+        </div>
+        <div className="expense-item__description">
+                <h2>{props.LocationOfExpenditure}</h2>
+            </div> */}
+            <div className="expense-item__price">{props.amount}</div></div>
+        
+       
+    );
+}
+export default ExpenseDetails;
