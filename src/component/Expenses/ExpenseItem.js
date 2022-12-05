5961d72a94efafda849ae7b6293e65f26e571c41
sonalighodake43772 <sonalighodake867>
2022-12-05 11:22:30 +0530

    usestate

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -1,19 +1,29 @@
 import ExpenseDate from "./ExpenseDate";
 //  import ExpenseDetails from './ExpenseDetails';
 import "./ExpenseItem.css";
-// import React from 'react';//required for older version of react
+import React,{useState} from 'react';//required for older version of react
 
 const ExpenseItem = (props) => {
+  const [Title,setTitle]=useState(props.title);
+  const[amount,setAmount]=useState(props.amount);
+
+
   const clickhandler=()=>
   {
 
-  console.log("clicked");
+    setTitle('updated');
+  console.log(Title);
   }
   const DeleteExpense=()=>
   {
 
   console.log("Deleted");
   }
+const changeExpense=()=>
+{
+
+  setAmount('100$');
+}
   return (
     // older version jsx code
     //   return React.createElement(
@@ -31,15 +41,16 @@ const ExpenseItem = (props) => {
       <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{Title}</h2>
       </div>
       <div className="expense-item__description">
         <h2>{props.LocationOfExpenditure}</h2>
       </div>
-      <div className="expense-item__price">{props.amount}</div>
+      <div className="expense-item__price">{amount}</div>
 
     <button onClick={clickhandler}>change title</button>
     <button onClick={DeleteExpense}>DeleteExpense</button>
+    <button onClick={changeExpense}>ChangeExpense</button>
     </div>
   );
 };
