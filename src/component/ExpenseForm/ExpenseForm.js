3768bbea41b10b76c41686dcb4b80da6b7cd8efe
sonalighodake43772 <sonalighodake867>
2022-12-06 11:44:56 +0530

    add submit event

--- src/component/ExpenseForm/ExpenseForm.js
+++ src/component/ExpenseForm/ExpenseForm.js
@@ -2,24 +2,34 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
-  const [enterdtitle, setEnteredtitle] = useState('');
-  const [enterdAmount, setEnteredamount] = useState('');
-  const [enterdDate, setEntereddate] = useState('');
+  const [enterdtitle, setEnteredtitle] = useState("");
+  const [enterdAmount, setEnteredamount] = useState("");
+  const [enterdDate, setEntereddate] = useState("");
 
   const ChangeTitle = (event) => {
     setEnteredtitle(event.target.value);
     // console.log('title chamnfe');
   };
   const ChangeAmount = (event) => {
-    
-   setEnteredamount(event.target.value); //event bject is used to target the value.
+    setEnteredamount(event.target.value); //event bject is used to target the value.
   };
   const ChangeDate = (event) => {
-  setEntereddate(event.target.value);
+    setEntereddate(event.target.value);
   };
+
+  const submithandler = (e) => {
+    e.preventDefault();
+    const expenseData = {
+      title: enterdtitle,
+      amount: enterdAmount,
+      date: new Date(enterdDate),
+    };
+    console.log(expenseData);
+  };
+
   //onchange is used when we enter something then it will print the changes.
   return (
-    <form>
+    <form onSubmit={submithandler}>
       <div className="new-expense__controls ">
         <div className="new-expense__controls ">
           <label>Title</label>
