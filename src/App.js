d6556baeeb4435a268815185baf10d1738d0daa3
sonalighodake43772 <sonalighodake867>
2022-12-09 11:34:18 +0530

    display data on screen

--- src/App.js
+++ src/App.js
@@ -1,73 +1,51 @@
 import ExpenseItem from "./component/Expenses/ExpenseItem";
 import NewExpense from "./component/ExpenseForm/NewExpense";
+import { useState } from "react";
 // import React from 'react';
-const App=()=> {
-  let expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      LocationOfExpenditure: "other",
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-      LocationOfExpenditure: "Electronics",
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      LocationOfExpenditure: "insurance",
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      LocationOfExpenditure: "furniture",
-    },
-  ];
 
+const old_expenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+    LocationOfExpenditure: "Electronics",
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+const App = () => {
+  const [expenses, setExpenses] = useState(old_expenses);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
   return (
     <div>
-     <NewExpense/>
-      {/* if data are multiple means 100 or more than 100 then iterate through the loop
-           expenses.map((item,i)=>
-            <ExpenseItem data={item}/>) */}
-
-      {/* if data are o to 5 */}
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-        LocationOfExpenditure={expenses[0].LocationOfExpenditure}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-        LocationOfExpenditure={expenses[1].LocationOfExpenditure}
-        />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-        LocationOfExpenditure={expenses[2].LocationOfExpenditure}
-        />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-        LocationOfExpenditure={expenses[3].LocationOfExpenditure}
-        />
-        
+      <NewExpense onaddexpense={addExpenseHandler} />
+      {expenses.map((item, i) => (
+        <ExpenseItem data={item} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
