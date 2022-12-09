d72a05f1c091bfa2e59cc7efd1d5e280ead2fe77
sonalighodake43772 <sonalighodake867>
2022-12-09 11:35:06 +0530

    add card

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -1,31 +1,23 @@
 import ExpenseDate from "./ExpenseDate";
+import Card from '../UI/card';
 //  import ExpenseDetails from './ExpenseDetails';
 import "./ExpenseItem.css";
-import React,{useState} from 'react';//required for older version of react
+// import React,{useState} from 'react';//required for older version of react
 
 const ExpenseItem = (props) => {
-  const [Title,setTitle]=useState(props.title);
-  const[amount,setAmount]=useState(props.amount);
-
-
-  const clickhandler=()=>
-  {
-
-    setTitle('updated');
-  console.log(Title);
-  }
-  const DeleteExpense=()=>
-  {
-
-  console.log("Deleted");
-  }
-const changeExpense=()=>
-{
-
-  setAmount('100$');
-}
   return (
-    // older version jsx code
+    <Card className="expense-item">
+      <ExpenseDate date={props.data.date} />
+
+      <div className="expense-item__description">
+        <h2>{props.data.title}</h2>
+      </div>
+      <div className="expense-item__price">{props.data.amount}</div>
+
+      {/* <button onClick={clickhandler}>change title</button>
+    <button onClick={DeleteExpense}>DeleteExpense</button> 
+  <button onClick={changeExpense}>ChangeExpense</button>*/}
+      {/* // older version jsx code
     //   return React.createElement(
     //     'div',
     //     {className:"expense-item"},
@@ -35,23 +27,8 @@ const changeExpense=()=>
     //  React.createElement('h1',{},'Expense 1'),
     //  React.createElement('h1',{},'Expense 1'))
 
-    //   );
-
-    <div className="expense-item">
-      <ExpenseDate date={props.date} />
-
-      <div className="expense-item__description">
-        <h2>{Title}</h2>
-      </div>
-      <div className="expense-item__description">
-        <h2>{props.LocationOfExpenditure}</h2>
-      </div>
-      <div className="expense-item__price">{amount}</div>
-
-    {/* <button onClick={clickhandler}>change title</button>
-    <button onClick={DeleteExpense}>DeleteExpense</button> */}
-    <button onClick={changeExpense}>ChangeExpense</button>
-    </div>
+    //   ); */}
+    </Card>
   );
 };
 export default ExpenseItem;
