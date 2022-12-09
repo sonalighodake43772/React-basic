b62dd1656d316bc6c73f5aff2002693a9b160c94
sonalighodake43772 <sonalighodake867>
2022-12-09 13:14:43 +0530

    add function

--- src/component/ExpenseForm/NewExpense.js
+++ src/component/ExpenseForm/NewExpense.js
@@ -1,13 +1,22 @@
 import ExpenseForm from "./ExpenseForm";
-import './NewExpense.css';
+import "./NewExpense.css";
 
-const NewExpense=()=>
-{
-    return (
-        <div className="new-expense">
-            <ExpenseForm/>
-        </div>
+const NewExpense = (props) => {
 
-    );
-}
+    const SaveExpenseDataHandler=(EnteredExpenseData)=>
+
+    {
+       const expensedata={
+        ...EnteredExpenseData,
+        id:Math.random().toString()
+       };
+    //    console.log(expensedata);
+    props.onaddexpense(expensedata);
+    }
+  return (
+    <div className="new-expense">
+      <ExpenseForm OnSaveExpenseData={SaveExpenseDataHandler} />
+    </div>
+  );
+};
 export default NewExpense;
